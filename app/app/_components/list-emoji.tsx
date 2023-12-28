44f3079525f91c029fd718c312463b5de1b5d734
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-12-28 12:29:00 -0600

    remove optimistic emoji due to pain

--- app/app/_components/list-emoji.tsx
+++ app/app/_components/list-emoji.tsx
@@ -11,22 +11,11 @@ export default function ListEmoji({
   emoji: string
   id: number
 }) {
-  console.log('emoji', emoji)
-
-  const [optimisticEmoji, changeOptimistic] = useOptimistic(
-    emoji,
-    (state, optimisticValue) =>
-      typeof optimisticValue === 'string' ? optimisticValue : state
-  )
-  console.log('optimistic', optimisticEmoji)
-
   function changeEmoji(emoji: string) {
-    // I think I'm supposed to use a transition here
     startTransition(() => {
-      changeOptimistic(emoji)
       updateEmoji({ emoji, id })
     })
   }
 
-  return <EmojiPopover onChange={changeEmoji}>{optimisticEmoji}</EmojiPopover>
+  return <EmojiPopover onChange={changeEmoji}>{emoji}</EmojiPopover>
 }
